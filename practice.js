f4e0ca084b4a2e94fc72d08d8ee0c83568216d6b
hongmoongi <hmoongi94@gmail.com>
2023-11-05 23:08:03 +0900

    json객체를 json문자열로 변환->json.stringfy(객체)

--- practice.js
+++ practice.js
@@ -1,3 +1,16 @@
 //61번. Date 객체를 사용해 현재 시간을 출력하는 코드를 작성하세요.
-  const currentdate = new Date();
-  console.log(currentdate)
+  // const currentdate = new Date();
+  // console.log(currentdate)
+
+//62번. Json 객체를 사용해 객체를 JSON 문자열로 변환하는 코드를 작성하세요.
+  // JavaScript 객체 생성
+    // const person = {
+      // firstName: "John",
+      // lastName: "Doe",
+      // age: 30,
+      // email: "john.doe@example.com"
+    // };
+    // console.log(person)
+  // JavaScript 객체를 JSON 문자열로 변환
+    // const jsonString = JSON.stringify(person);
+    // console.log(jsonString);
