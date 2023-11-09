9fd24a6969b01d1fc0952e524524038b7192b5d3
HongMoonGI <hmoongi94@gmail.com>
2023-11-09 19:53:30 +0900

    flat(): 깊이의 뜻 배열을 몇 겹씩 벗긴다의 뜻

--- practiceArray.js
+++ practiceArray.js
@@ -36,7 +36,8 @@ const testNumbers = [1,2,3,4,5,6,7,8,9]
 //* includes()메서드는 특정 요소가 있는지 판별할 때 사용 반환값은 true, false로 반환한다.
 
 //* flatMap()메서드: 평탄화한다는 말은 하나의 배열로 만든다는 말. map과 flatMap을 비교해보면 차이를 이해하는데 도움이 된다.
-//* flat()
+//* flat()메서드: array.flat([depth])
+//* flat()을 한번 쓸 때마다 배열의 한겹씩 벗겨진다. 수행할깊이의 숫자를 높이면 배열을 한번에 여러겹 벗긴다. 초기값은 1로 한겹씩 벗겨짐.
 
 
 
@@ -226,7 +227,14 @@ const testNumbers = [1,2,3,4,5,6,7,8,9]
 
 
   //? 24. 지정된 깊이까지 재귀적으로 평탄화하여 새 배열을 생성하는 메서드는 무엇인가요?
-  //* flat()메서드
+  //* flat()메서드: 작성법 -> array.flat([depth])
+  const nestedArray = [1, [2, [3, [4]], 5]];
+
+  const flattenedArray = nestedArray.flat(0);
+
+  console.log(flattenedArray);
+
+
 
   //* 13~15번 테스트
   // const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
